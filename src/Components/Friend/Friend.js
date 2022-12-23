b9a259d9eb6822ee0ce942a2a19a9df9b39d1c20
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-23 22:33:55 +0530

    navigate added

---
+++ src/Components/Friend/Friend.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useNavigate} from'react-router-dom'
+
+const Friend = (props) => {
+    const {name,username,id} = props.friend;
+    const navigate = useNavigate();
+    const showFrinedDetail = () =>{
+        const path = `/friend/${id}`;
+        navigate(path)
+    }
+    return (
+        <div>
+            <h2>name : {name}</h2>
+            <button onClick={showFrinedDetail}>{username} id:{id}</button>
+        </div>
+    );
+};
+
+export default Friend;
--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const [Friends,setFriends] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+           .then(res => res.json())
+           .then(data => setFriends(data));
+    },[])
     return (
         <div>
             <h1>hello Friends ! how are you ?</h1>
-            <p>O hello all of may Friends </p>
+                        {/* <p>O hello all of my Friends : {Friends.length} </p> */}
+            {
+                Friends.map(friend => <Friend
+                    key={friend.id}
+                    friend ={friend} 
+                ></Friend>)
+            }
         </div>
     );
 };
