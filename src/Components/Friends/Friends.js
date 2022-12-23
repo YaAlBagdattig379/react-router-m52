98b2031f9fb4767f99557ee8dea6d4bdd9584b53
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-23 20:22:50 +0530

    first commit from react-router

---
+++ src/Components/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h1>hello Friends ! how are you ?</h1>
+        </div>
+    );
+};
+
+export default Friends;
