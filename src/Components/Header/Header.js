c6c020ded9422df311f4b77592bfbcf1f44c8178
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-25 02:17:31 +0530

    added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -15,9 +15,11 @@ const Header = () => {
                 {/* <a href="/">Home</a> // normal exp>>>>... 1
                 <a href="/friends">Friends</a>
                 <a href="/about">about</a> */}
-                {/* <Link to="/">Home</Link> second exp>>>....2     Link usage
+                  {/*  exp>>>....2 Link usage*/}
+                {/* <Link to="/">Home</Link> second 
                 <Link to="/friends">Friends</Link>
                 <Link to="/about">about</Link> */}
+                {/*  exp>>>....3 NavLink usage*/}
                 <NavLink style={NavLinkCSS} to="/">Home</NavLink>
                 <NavLink style={NavLinkCSS} to="/friends">Friends</NavLink>
                 <NavLink style={NavLinkCSS} to="/about">about</NavLink>
